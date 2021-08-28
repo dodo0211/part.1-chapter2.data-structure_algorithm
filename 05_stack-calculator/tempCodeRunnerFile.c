fd326662b79184a144193b55201e543ed9ace598
dodo0211 <minar2010@naver.com>
2021-08-28 18:13:25 +0900

    스택을 활용한 계산기 만들기_C
    - 후위 표기법으로 변환

---
+++ 05_stack-calculator/tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+
+    // for(int i=0; i<strlen(b) -1; i++) { // 마지막은 항상 공백이 들어가므로 -1 (line 55참고)
+    //     if(b[i] == ' ') size++;
+    // }
+    // char *ptr2 = strtok(b, " ");
+    // for(int i=0; i<size; i++) {
+    //     strcpy(input[i], ptr2);
+    //     ptr2 = strtok(NULL, " ");
+    // }
+    // calculate(&stack, input, size);
